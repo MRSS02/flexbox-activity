34aba557120359f65f6bc0b5cfee2bfcdfe268f6
MRSS <mateusyasd@hotmail.com>
2021-03-09 22:01:26 -0300

    got to make it workinggit add . git add .  (i think)

--- script.js
+++ script.js
@@ -4,150 +4,80 @@ function enviar(x) {
     alert(`Sua ${x} foi enviada!`)
 }
 
+function buttonUpdate(tela) {
 
-function change(tela) {
+  document.title=tela;
+  function branco(botao) {
+      botao.className = `button`;
+  }
 
-    onRRpage = false
-    document.title=tela;
-    document.body.style=`background-image: linear-gradient(#a95aec, #40e0d0, #ffb6c1); background-size:cover;`
+  [...document.getElementsByName("button")].forEach(branco);
 
-    function branco(botao) {
-        botao.className = `button`;
-    }
-
-    [...document.getElementsByName("button")].forEach(branco);
-
-
-    document.getElementById(tela).className += ` selected`
-
-    switch(tela) {
-
-        case 'MAIN':
-
-            document.getElementById("pag").innerHTML=
-            `
-            <h1 class="content">Bem vindos!!!</h1>
-
-            <h2 class="content">Esta página tem botões que vão levar para diferentes interatividades. <br> Explore todas as opções!</h2>
-            `;
-
-        break;
-
-
-
-        case 'QUIZ':
-
-            document.getElementById("pag").innerHTML=
-            `
-            <h1 class="content">Responda o que você acredita estar correto.</h1>
-
-            <div class="perguntas">
-
-            <h2>Qual a data de lançamento de Persona 5 Scramble?</h2>
-
-            <input type="month">
-
-            </div>
-
-            <div class="perguntas">
-
-            <h2>Quantos personagens existem no Super Smash Bros. de Nintendo 64 no total?</h2>
-
-            <input type="number">
-
-            </div>
-
-            <div class="perguntas">
-
-            <h2>Quem matou L no Death Note?</h2>
-
-            <input type="radio" name="brr">Ryuk</input>
-            <input type="radio" name="brr">Light</input>
-            <input type="radio" name="brr">Rem</input>
-            <input type="radio" name="brr">Misa</input>
-
-
-            </div>
-
-            <div class="perguntas">
-
-            <h2>Qual o próximo personagem 5 star para Genshin Impact?</h2>
-
-            <input type="text">
-
-            </div>
-
-
-            <button onclick="enviar('resposta')" style="margin-top:40px">Enviar</button>
-
-
-            `;
-
-        break;
-
-
-
-        case 'UPLOAD':
-
-            document.getElementById("pag").innerHTML=
-            `
-            <h1 class="content">Envie uma atividade para maior análise.</h1>
-
-            <input type="file">
-
-            <button onclick="enviar('atividade')"> Enviar </button>
-
-            `;
-
-        break;
-
-
-
-        case 'CONFESSION':
-
-            document.getElementById("pag").innerHTML=
-            `
-            <h1 class="content">Confesse algo sobre si mesmo em 2020.</h1>
-
-                <input type="text" size=150px>
-
-                <button onclick="enviar('confissão')"> Enviar </button>
-
-            `;
-
-        break;
-
-
-
-        case 'GETSTICKBUGGED':
-            onRRpage = true
-            document.getElementById("pag").innerHTML=
-            `
-
-            <div id="get">
-            <h1 class="content" style='margin-top:30vh'>Leia o título da página.</h1>
-
-
-            </div>
-
-            `;
-
-            setTimeout(function(){document.getElementById("get").innerHTML=`
-            <h1 class="content" style='margin-top:0'>Leia o título da página.</h1>;
-            <iframe width="500" height="500" src="stick.gif"></iframe>
-            `},5000)
-
-            setTimeout(function(){
-                if (onRRpage) {
-                  document.body.style='background-image: url("rolled.gif"); background-size:auto';
-                  document.title="GETSTICKROLLED";
-                  document.getElementsByClassName("content")[0].style=`color:white`;
-                }
-            },9500)
-
-
-
-        break;
-    }
-
+  document.getElementById(tela).className += ` selected`
 }
+
+function update() {
+   var window_top = $(window).scrollTop();
+   var pag1_top = $('#pag1').offset().top;
+   var pag2_top = $('#pag2').offset().top;
+   var pag3_top = $('#pag3').offset().top;
+   var pag4_top = $('#pag4').offset().top;
+   console.log(window_top)
+
+   if (window_top < pag1_top * (0.7)) {
+     buttonUpdate("MAIN")
+     document.getElementById("get").innerHTML=`
+     <h1 class="content">Leia o título da página.</h1>`
+     onRRpage = false
+     document.body.style=`
+     background-image: linear-gradient(#a95aec, #40e0d0, #ffb6c1); background-size:cover;`
+   }
+   else if (window_top < pag2_top * (0.8)) {
+     buttonUpdate("QUIZ")
+     document.getElementById("get").innerHTML=`
+     <h1 class="content">Leia o título da página.</h1>`
+     onRRpage = false
+     document.body.style=`
+     background-image: linear-gradient(#a95aec, #40e0d0, #ffb6c1); background-size:cover;`
+   }
+   else if (window_top < pag3_top * (0.9)) {
+     buttonUpdate("UPLOAD")
+     document.getElementById("get").innerHTML=`
+     <h1 class="content">Leia o título da página.</h1>`
+     onRRpage = false
+     document.body.style=`
+     background-image: linear-gradient(#a95aec, #40e0d0, #ffb6c1); background-size:cover;`
+   }
+   else if (window_top < pag4_top * (0.9)) {
+       buttonUpdate("CONFESSION")
+       document.getElementById("get").innerHTML=`
+       <h1 class="content">Leia o título da página.</h1>`
+       onRRpage = false
+       document.body.style=`
+       background-image: linear-gradient(#a95aec, #40e0d0, #ffb6c1); background-size:cover;`
+   }
+   else {
+      onRRpage = true
+      buttonUpdate("GETSTICKBUGGED")
+      if (!onRRpage) document.getElementById("get").innerHTML=`
+      <h1 class="content">Leia o título da página.</h1>`
+      setTimeout(function(){document.getElementById("get").innerHTML=`
+      <h1 class="content" style='margin-top:0'>Leia o título da página.</h1>;
+      <iframe width="500" height="500" src="stick.gif"></iframe>
+      `},5000)
+
+      setTimeout(function(){
+        if (onRRpage) {
+            document.body.style='background-image: url("rolled.gif"); background-size:auto';
+            document.title="GETSTICKROLLED";
+            document.getElementById("content").style=`color:white`;
+         }
+      },9500)
+
+  }
+}
+
+$(function() {
+  $(window).scroll(update);
+  update();
+ });
